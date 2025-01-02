a91573174656ea6782c4f07ccf5927682d0a5c6c
Chaithanya K S <127503791+chaithanya-ks2@users.noreply.github.com>
2025-01-02 15:02:11 +0530

    Update readCSVcol.py

--- S1/Python/readCSVcol.py
+++ S1/Python/readCSVcol.py
@@ -1,6 +1,7 @@
 myFile = open("random_data.csv", "r")
 lines = myFile.readlines()
 lines = [line.strip().split(",") for line in lines]
+myFile.close()
 
 for i in range(len(lines[0])):
     for j in range(len(lines)):
