268617b781e963fa975df4261770c8ec31f0b2cb
Chaithanya K S <127503791+chaithanya-ks2@users.noreply.github.com>
2025-01-06 17:25:49 +0530

    Update 15_dictionaryMerge.py

--- S1/Python/15_dictionaryMerge.py
+++ S1/Python/15_dictionaryMerge.py
@@ -4,7 +4,7 @@ for key in keys:
     value = input(f"Enter the value for {key}: ")
     dict1[key] = value
 
-keys = input("Enter the keys for first dictionary: ").split()
+keys = input("Enter the keys for second dictionary: ").split()
 dict2 = {}
 for key in keys:
     value = input(f"Enter the value for {key}: ")
