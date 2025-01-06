a0654e90625cd95658d6dbb299e73e30fec44c38
Chaithanya K S <127503791+chaithanya-ks2@users.noreply.github.com>
2025-01-07 02:00:56 +0530

    Update readCSVcol.py : print specific columns only

--- S1/Python/readCSVcol.py
+++ S1/Python/readCSVcol.py
@@ -1,9 +1,12 @@
 myFile = open("random_data.csv", "r")
+
 lines = myFile.readlines()
 lines = [line.strip().split(",") for line in lines]
-myFile.close()
 
-for i in range(len(lines[0])):
-    for j in range(len(lines)):
-        print(lines[j][i], end=" ")
-    print()
+headings = lines[0]
+print("The columns are:",headings)
+col = input("Enter the column to be printed: ")
+
+index = headings.index(col)
+for i in range(len(lines)):
+    print(lines[i][index], end=" ")
