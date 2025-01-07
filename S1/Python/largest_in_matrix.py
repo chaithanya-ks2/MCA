199917a1fcf2ae8eef66693c668d80cf4c055f43
Chaithanya K S <127503791+chaithanya-ks2@users.noreply.github.com>
2025-01-07 17:38:29 +0530

    Internal lab

---
+++ S1/Python/largest_in_matrix.py
@@ -0,0 +1,32 @@
+class Location:
+    def __init__(self, no_of_rows, no_of_columns):
+        self.m = no_of_rows
+        self.n = no_of_columns
+        self.matrix = self.read_values()
+
+    def read_values(self):
+        matrix = []
+        for i in range(self.m):
+            row = []
+            for j in range(self.n):
+                val = int(input(f"Enter the value at [{i}][{j}]: "))
+                row.append(val)
+            matrix.append(row)
+
+        return matrix
+    
+    def find_max(self):
+        max_val = self.matrix[0][0]
+        for i in range(self.m):
+            for j in range(self.n):
+                if self.matrix[i][j] > max_val:
+                    max_val = self.matrix[i][j]
+                    loc = (i, j)
+        
+        print(f"Largest value in the matrix is {max_val}")
+        print(f"{max_val} is located at index {loc}")
+
+
+a, b = map(int, input("Enter the number of rows and columns: ").split())
+obj1 = Location(a, b)
+obj1.find_max()
---
+++ S1/Python/states.py
@@ -0,0 +1,47 @@
+states = input("Enter the states: ").split()
+state_dict = {state: len(state) for state in states}
+
+# sort dictionary by value
+def sort_dict(dict):
+    values = list(state_dict.values())
+    values.sort()
+
+    state_dict_sorted = {}
+
+    for val in values:
+        for state, length in state_dict.items():
+            if val == length:
+                state_dict_sorted[state] = length
+    
+    return state_dict_sorted
+
+# writing into csv file
+def write_csv(state_dict_sorted):
+    with open("states.csv", "w") as myfile:
+        for state, length in state_dict_sorted.items():  
+            myfile.write(",".join([state, str(length)]))
+            myfile.write("\n")
+
+#reading from csv file
+def read_csv():
+    with open("states.csv", "r") as myfile:
+        lines = myfile.readlines()
+        lines = [line.strip() for line in lines]
+        for line in lines:
+            print(line, end=" ")
+            print()
+
+sorted = sort_dict(state_dict)
+write_csv(sorted)
+read_csv()
+
+# updating state list
+ans = input("Do you want to update states (y/n): ")
+if ans == "y":
+    state = input("Enter the state: ")
+    state_dict[state] = len(state)
+
+    sorted = sort_dict(state_dict)
+    write_csv(sorted)
+    read_csv()
+
