90597c6f0ecda73a5ed9d468d7c21ae4d0dc49d2
Chaithanya K S <127503791+chaithanya-ks2@users.noreply.github.com>
2025-03-18 10:11:53 +0530

    Add files via upload

---
+++ S2/OOPS lab/Day 4/StringClass.java
@@ -0,0 +1,15 @@
+public class StringClass {
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println(s);
+
+        String sh = new String("World");
+        System.out.println(s+" "+sh);
+
+        int leng=sh.length()+s.length();
+        System.out.println(leng);
+
+
+    }
+}
+
